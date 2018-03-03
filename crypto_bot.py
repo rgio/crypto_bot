2a27086bca4940a8dabc3f848ee4980b5438a381
John Philbin <jphilbin01@gmail.com>
2018-03-03 12:10:52 -0800

    updated cnn_model, testing validation sets, hparam

--- crypto_bot.py
+++ crypto_bot.py
@@ -3,12 +3,14 @@ from __future__ import division
 from __future__ import print_function
 
 # Imports of general code
+import sys
 import datetime
 import tempfile
 import pathlib
-import pdb
+import argparse
 import numpy as np
 import tensorflow as tf
+import pdb
 
 # Imports of our own code
 import hparams as hp
@@ -17,14 +19,15 @@ import cnn
 import loss_value as lv
 import print_results as prnt
 import functions as fn
-import pdb
 
-
-def main():
-	# Load training and eval data
+def main(_):
+	# Set up the models hyperparameters
 	hparams = hp.set_hyperparameters()
+	if FLAGS.hparams: # override default hparams if command line hparams exist
+		hparams.parse(FLAGS.hparams)
+	# Load training and eval data
 	try:
-		input_array = pdata.read_data()
+		input_array = pdata.read_data(FLAGS.data_dir)
 	except:
 		pdata.fetch_data()
 		input_array = pdata.read_data()
@@ -90,11 +93,7 @@ def main():
 	prnt.print_hyperparameters(hparams, path_to_model_dir)
 	path_to_final_model = path_to_model_dir + 'cnn_model.ckpt'
 	path_to_best_model = path_to_model_dir + 'cnn_best_model.ckpt'
-	best_val_value = 0.0 # used to save 
-
-	# Random weights for 1st training step
-	random_weights = np.random.rand(hparams.batch_size, hparams.num_coins+1)
-	val_weights = np.random.rand(validation_labels.shape[0], validation_labels.shape[1])
+	best_val_value = 0.0 # used to save the best model
 
 	# Stores our portfolio weights
 	memory_array = np.random.rand(train_data.shape[0], hparams.num_coins+1)
@@ -119,83 +118,52 @@ def main():
 				train_accuracy = accuracy.eval(feed_dict={input_prices: batch[0], labels: batch[1], 
 					init_weights: input_weights, batch_size: hparams.batch_size, keep_prob: 1.0})
 				print('Step = %d\nBatch = %d\nTrain_accuracy = %g\nTrain_value = %g' % (i, batch[2], train_accuracy, train_value))
-			if i % 10000 == 0:
-				val_weights = weights.eval(feed_dict={input_prices: validation_data, labels: validation_labels,
-						init_weights: val_weights, batch_size: validation_labels.shape[0], keep_prob: 1.0})
-				final_pvm = final_value.eval(feed_dict={input_prices: validation_data, labels: validation_labels,
-					init_weights: val_weights, batch_size: validation_labels.shape[0], keep_prob: 1.0})
-				val_weights = weights.eval(feed_dict={input_prices: validation_data, labels: validation_labels,
-					init_weights: val_weights, batch_size: validation_labels.shape[0], keep_prob: 1.0})
-				print('Step %d, validation_size %d, validation_value %g' % (i, validation_labels.shape[0], final_pvm))
-				if (final_pvm > best_val_value):
+			if i % 10000 == 0 or i == hparams.num_training_steps-1:
+				validation_weights = np.zeros((1, validation_labels.shape[1]))
+				validation_weights[0,0]  = 1.0
+				v = np.ones((validation_labels.shape[0]))
+				portfolio_value = 1.0
+				for i in range(0, validation_labels.shape[0]):
+					v_labels = np.reshape(validation_labels[i,:], (1, validation_labels.shape[1]))
+					v_data = np.reshape(validation_data[i,:], (1, validation_data.shape[1], validation_data.shape[2], validation_data.shape[3]))
+					v[i] = final_value.eval(feed_dict={input_prices: v_data, labels: v_labels, 
+								init_weights: validation_weights, batch_size: 1, keep_prob: 1.0})
+					portfolio_value = portfolio_value*v[i]
+					validation_weights = weights.eval(feed_dict={input_prices: v_data, labels: v_labels, 
+						init_weights: validation_weights, batch_size: 1, keep_prob: 1.0})
+				print('Step %d, validation_size %d, validation_value %g' % (i, validation_labels.shape[0], portfolio_value))
+				if (portfolio_value > best_val_value):
 					save_path_best_model = saver.save(sess, path_to_best_model) 
-					best_val_value = final_pvm
+					best_val_value = portfolio_value
+					np.savetxt(path_to_model_dir + 'proper_validation_returns.out', v, fmt='%.8f', delimiter=' ')
 					print('new best validation value, best model weights saved in %s\n' % save_path_best_model)
 			train_step.run(feed_dict={input_prices: batch[0], labels: batch[1], 
 				init_weights: input_weights, batch_size: hparams.batch_size, keep_prob: hparams.dropout_keep_prob})
 
-		# Save the results
+		# Save the model
 		save_path_final_model = saver.save(sess, path_to_final_model)
 		print('The final model weights are saved in %s' % save_path_final_model)
 
-		# Print the validation results
-		random_weights = np.random.rand(validation_labels.shape[0], validation_labels.shape[1])
-		input_weights = weights.eval(feed_dict={input_prices: validation_data, labels: validation_labels, 
-		 		init_weights: random_weights, batch_size: validation_labels.shape[0], keep_prob: 1.0})
-		print('The accuracy on the validation set is %g' % accuracy.eval(feed_dict={input_prices: validation_data, 
-			labels: validation_labels, init_weights: input_weights, batch_size: validation_labels.shape[0], keep_prob: 1.0}))
-		print('The accuracy (with val_weights) on the validation set is %g' % accuracy.eval(feed_dict={input_prices: validation_data, 
-			labels: validation_labels, init_weights: val_weights, batch_size: validation_labels.shape[0], keep_prob: 1.0}))		
-		final_pvm = final_value.eval(feed_dict={input_prices: validation_data, labels: validation_labels, 
-		 	init_weights: input_weights, batch_size: validation_labels.shape[0], keep_prob: 1.0})
-		pvm = value.eval(feed_dict={input_prices: validation_data, labels: validation_labels, 
-			init_weights: input_weights, batch_size: validation_labels.shape[0], keep_prob: 1.0})
-		portfolio_weights = weights.eval(feed_dict={input_prices: validation_data, labels: validation_labels, 
-			init_weights: input_weights, batch_size: validation_labels.shape[0], keep_prob: 1.0})
-		prnt.print_model_results(final_pvm, pvm, portfolio_weights, path_to_model_dir, 'validation')
-		if (final_pvm > best_val_value):
-			saver.save(sess, path_to_best_model)
-		
-		# Print the test results
-		random_weights = np.random.rand(test_labels.shape[0], test_labels.shape[1])
-		input_weights = weights.eval(feed_dict={input_prices: test_data, labels: test_labels, 
-		 		init_weights: random_weights, batch_size: test_labels.shape[0], keep_prob: 1.0})
-		print('The accuracy on the test set is %g' % accuracy.eval(feed_dict={input_prices: test_data, 
-			labels: test_labels, init_weights: input_weights, batch_size: test_labels.shape[0], keep_prob: 1.0}))
-		final_pvm = final_value.eval(feed_dict={input_prices: test_data, labels: test_labels, 
-			init_weights: input_weights, batch_size: test_labels.shape[0], keep_prob: 1.0})
-		pvm = value.eval(feed_dict={input_prices: test_data, labels: test_labels, 
-			init_weights: input_weights, batch_size: test_labels.shape[0], keep_prob: 1.0})
-		portfolio_weights = weights.eval(feed_dict={input_prices: test_data, labels: test_labels, 
-			init_weights: input_weights, batch_size: test_labels.shape[0], keep_prob: 1.0})
-		prnt.print_model_results(final_pvm, pvm, portfolio_weights, path_to_model_dir, 'test')
-		#print('Test trading period = %d steps and %.2f days' % (price_change.shape[0], price_change.shape[0]/48.0))
+		# Proper test set test
+		test_weights = np.zeros((1, test_labels.shape[1]))
+		test_weights[0,0]  = 1.0
+		t = np.ones((test_labels.shape[0]))
+		portfolio_value = 1.0
+		for i in range(0, test_labels.shape[0]):
+			t_labels = np.reshape(test_labels[i,:], (1, test_labels.shape[1]))
+			t_data = np.reshape(test_data[i,:], (1, test_data.shape[1], test_data.shape[2], test_data.shape[3]))
+			t[i] = final_value.eval(feed_dict={input_prices: t_data, labels: t_labels, 
+						init_weights: test_weights, batch_size: 1, keep_prob: 1.0})
+			portfolio_value = portfolio_value*t[i]
+			test_weights = weights.eval(feed_dict={input_prices: t_data, labels: t_labels, 
+				init_weights: test_weights, batch_size: 1, keep_prob: 1.0})
+		np.savetxt(path_to_model_dir + 'proper_test_returns.out', t, fmt='%.8f', delimiter=' ')
+		print('The (proper) final test set value multiplier is %.8f' % portfolio_value)
 
-
-		"""while True:
-			data, labels = get_current_window()
-			print(data)
-			print(labels)
-			weights = weights.eval(feed_dict={input_prices: data, labels: labels, 
-				init_weights: input_weights, keep_prob: 1.0})
-			get_new_portfolio(weights)
-			time.sleep(1800)"""
-
-		# Proper validation test
-		# validation_weights = np.zeros((1, validation_labels.shape[1]))
-		# validation_weights[0,0]  = 1.0
-		# v = np.ones((validation_labels.shape[0]))
-		# portfolio_value = 1.0
-		# for i in range(0, validation_labels.shape[0]):
-		# 	v_labels = np.reshape(validation_labels[i,:], (1, validation_labels.shape[1]))
-		# 	v_data = np.reshape(validation_data[i,:], (1, validation_data.shape[1], validation_data.shape[2], validation_data.shape[3]))
-		# 	v[i] = final_value.eval(feed_dict={input_prices: v_data, labels: v_labels, 
-		# 				init_weights: validation_weights, batch_size: 1, keep_prob: 1.0})
-		# 	portfolio_value = portfolio_value*v[i]
-		# 	validation_weights = weights.eval(feed_dict={input_prices: v_data, labels: v_labels, 
-		#   		init_weights: validation_weights, batch_size: 1, keep_prob: 1.0})
-		# np.savetxt('tmp/proper_validation_returns.out', v, fmt='%.8f', delimiter=' ')
-		# print('The (proper) final validation set value multiplier is %.8f' % portfolio_value)
 
 if __name__ == '__main__':
-	main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--data_dir', type=str, default='data/', help='Directory for storing input data')
+	parser.add_argument('--hparams', type=str, default=None, help='Comma separated list of "name=value" pairs.')
+	FLAGS, unparsed = parser.parse_known_args()
+	tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
--- hparams.py
+++ hparams.py
@@ -14,15 +14,21 @@ def set_hyperparameters():
 		window_size=50,
 		stride=1,
 		batch_size=100,
+		batch_sampling_method='random_geometric', # options: random_geometric, random_uniform, systematic_uniform in price_data.py
 		num_training_steps=200000,
 		learning_rate=2e-4,
-		geometric_decay=2.0, # the large geometric_decay is the more recent times will be selected in training
+		geometric_decay=2.0, # larger geometric_decay leads to more recent times being selected more during training
 		num_input_channels=4, # high, open, volume, dp/dt
-		num_conv1_features=2,
-		num_conv2_features=20,
-		num_fc1_neurons=12,
+		conv_layers_separable=True,
+		len_conv1_filters=3,
+		num_conv1_features=8,
+		num_conv2_features=32,
 		dropout_keep_prob=0.5,
-		# num_fc2_neurons = len(hparams.coin_pairs) + 1 (+1 needed for BTC)
+		model_ending='one_fc_layer', # options: two_fc_layers, one_fc_layer, third_conv_layer in cnn.py
+		num_fc1_neurons=12, # only for option two_fc_layers; it is set to num_coins for one_fc_layer in cnn.py
+		len_conv3_filters=1, # only used in third_conv_layer option = 1 for 1x1 convolutions
+		# num_fc2_neurons = len(hparams.coin_pairs) 
 		)
 	hparams.add_hparam("num_coins", len(hparams.coin_pairs))	
 	return hparams
+
